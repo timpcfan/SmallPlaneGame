7bfd9929595e3b14b50bdcd74871f869db55d811
婉鱼 <291785462@qq.com>
2017-10-16 20:29:34 +0800

    完善了游戏开始界面

--- src/cn/edu/scau/cmi/glz/ViewBuilder.java
+++ src/cn/edu/scau/cmi/glz/ViewBuilder.java
@@ -9,9 +9,33 @@ public class ViewBuilder {
 	public static GameModel buildMainView(GameFrame frame) {
 		GameModel model = new GameModel();
 		// TODO 向model中添加实体构成主界面
+		TextEntity text1 = TextEntity.buildTextEntity("Play", 30,
+				frame.getCanvasWidth() / 2, frame.getCanvasHeight() / 2,
+				VisHelper.DeepOrange);
+        model.addText(text1);
+        TextEntity text2 = TextEntity.buildTextEntity("Close", 30,
+        		frame.getCanvasWidth() / 2, frame.getCanvasHeight() / 2+125,
+				VisHelper.Sky);
+        model.addText(text2);
+        TextEntity text3 = TextEntity.buildTextEntity("Welcome to Tiny Plane!", 30,
+				frame.getCanvasWidth() / 2, frame.getCanvasHeight() / 2-100,
+				VisHelper.Orange);
+        model.addText(text3);
+
+// 添加图形
+        ShapeEntity ellipse1 = ShapeEntity.buildEllipseEntity(150, 325, 200, 110, VisHelper.Teal, true);
+		model.addShape(ellipse1);
+		ShapeEntity ellipse2 = ShapeEntity.buildEllipseEntity(150, 450, 200, 110, VisHelper.Teal, true);
+		model.addShape(ellipse2);
+// 添加图片
+        model.addImage(new ImageEntity(300, 100, "resources/stone1.png"));
+        model.addImage(new ImageEntity(20, 600, "resources/ufo.png"));
+        model.addImage(new ImageEntity(20, 600, "resources/bullet.png"));
+        model.addImage(new ImageEntity(230, 600, "resources/bullet.png"));
+
 		
-		return model;
-	}
+	return model;
+    }
 	
 	/**
 	 * 生成游戏结束界面的数据模型
@@ -61,8 +85,8 @@ public class ViewBuilder {
 	 * 此main方法用于测试界面
 	 */
 	public static void main(String[] args) {
-		GameFrame frame = new GameFrame("测试用界面", 500, 800);
-		GameModel model = buildDemoView(frame); // 这里将model赋值成需要测试的界面数据模型Model
+		GameFrame frame = new GameFrame("游戏开始界面", 500, 800);
+		GameModel model = buildMainView(frame); // 这里将model赋值成需要测试的界面数据模型Model
 		frame.render(model);
 	}
 
